2050abc6992945989607318a1fc08506f93daf30
Ablo224 <ablobalde92@gmail.com>
2019-10-22 21:07:26 +0200

    implementation de l'enregistrement d'un operande

--- CalculatriceRPN/src/CalculatriceRPN/MoteurRPN.java
+++ CalculatriceRPN/src/CalculatriceRPN/MoteurRPN.java
@@ -3,9 +3,25 @@ package CalculatriceRPN;
 import java.util.*;
 
 public class MoteurRPN {
+	
+	private Stack<Double> _pile;
+	public final double MAX_VALUE = /*Double.MAX_VALUE;*/ 1000000;
+	public final double MIN_VALUE = /*Double.MIN_VALUE;*/ 0;
 
 	public MoteurRPN() {
-		// TODO Auto-generated constructor stub
+		_pile = new Stack<Double>();
 	}
-
+	
+	public void ajouterOperande(double operande)throws OperandeTropPetitException, OperandeTropGrandException
+	{
+		if(operande < MIN_VALUE)
+		{
+			throw new OperandeTropPetitException();
+		}
+		else if(operande > MAX_VALUE)
+		{
+			throw new OperandeTropGrandException();
+		}
+		_pile.push(operande);
+	}
 }
